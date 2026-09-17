efbbd17a7d8d71e90a3fd16084f63f0b86be3917
Victor Jost <victor.jost@arolo-solutions.com>
2026-09-17 16:53:40 +0200

    from django to react + fastAPI

--- park_management/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Attraction, AttractionVisit, Billet, QueueEntry
-
-admin.site.register(Billet)
-admin.site.register(Attraction)
-admin.site.register(QueueEntry)
-admin.site.register(AttractionVisit)
--- park_management/urls.py
+++
@@ -1,9 +0,0 @@
-"""Routes every page of the application, one included URLconf per page."""
-from django.urls import include, path
-
-urlpatterns = [
-    path("", include("park_management.pages.tickets.urls")),
-    path("attractions/", include("park_management.pages.attractions.urls")),
-    path("console/", include("park_management.pages.console.urls")),
-    path("compte/", include("park_management.pages.accounts.urls")),
-]
